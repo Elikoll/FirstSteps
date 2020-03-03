b26ff331acd025e6d2ad9230028ca9f8f8964c41
Simon Plakolb <s.plakolb@gmail.com>
2020-03-03 11:21:13 +0100

    Wir geben nun mehrere Zeilen aus

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld Campus");
+        System.out.println("HelloWorld");
+        System.out.println("Campus");
+        // Campus
+        // HelloWorld
+    }
+}
