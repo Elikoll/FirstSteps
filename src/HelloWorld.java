d20bcdf952211fe24c3df234b894df9408700dcb
Simon Plakolb <s.plakolb@gmail.com>
2020-03-03 16:11:24 +0100

    Wir können nun bereits etwas Arithmetik ausüben und das Ergebnis ausgeben

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,12 @@ import java.util.LinkedList;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld Campus");
         System.out.println("HelloWorld");
-        System.out.println("Campus");
+        // System.out.println("HelloWorld");
+        int i = 2;
+        double d = 1.5;
+        double x = i*d + d;
+        System.out.println("Campus 0"+x);
         // CampusBla
         // HelloWorld
     }
